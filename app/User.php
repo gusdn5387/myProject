bb14aadc0474a68ca1559adbdff32a5dba5f1b51
HyeonWoo Park <gusdn5387@naver.com>
2019-01-25 14:44:20 +0900

    모델 간의 관계

--- app/User.php
+++ app/User.php
@@ -8,23 +8,26 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    public function posts() {
+        return $this->hasMany('App\Post');
+    }
+    // use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
+    // /**
+    //  * The attributes that are mass assignable.
+    //  *
+    //  * @var array
+    //  */
+    // protected $fillable = [
+    //     'name', 'email', 'password',
+    // ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
+    // /**
+    //  * The attributes that should be hidden for arrays.
+    //  *
+    //  * @var array
+    //  */
+    // protected $hidden = [
+    //     'password', 'remember_token',
+    // ];
 }
