2c171e6dc3b30fa7f37b45553b452e75316ac16e
HyeonWoo Park <gusdn5387@naver.com>
2019-01-25 15:34:44 +0900

    입력값 유효성 검사

--- routes/web.php
+++ routes/web.php
@@ -54,6 +54,25 @@ Route::get('posts', function() {
 
 Route::resource('posts.comments', 'PostsCommentController');
 
+Route::post('posts', function(\Illuminate\Http\Request $request) {
+    $rule = [
+        'title' => ['required'],
+        'body' => ['required', 'min:10']
+    ];
+
+    $validator = Validator::make($request->all(), $rule);
+
+    if($validator->fails()) {
+        return redirect('posts/create')->withErrors($validator)->withInput();
+    }
+
+    return 'Valid & proceed to next job ~';
+});
+
+Route::get('posts/create', function() {
+    return view('posts.create');
+});
+
 // Route::get('home', [
 //     'middleware' => 'auth',
 //     function() {
