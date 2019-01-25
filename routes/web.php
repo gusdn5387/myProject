b8b7ba4db6c8fcca9f7669411163f96b294b6d19
HyeonWoo Park <gusdn5387@naver.com>
2019-01-25 14:27:08 +0900

    사용자 인증

--- routes/web.php
+++ routes/web.php
@@ -56,3 +56,30 @@ Route::resource('posts', 'PostsController');
 // ]);
 
 Route::resource('posts.comments', 'PostsCommentController');
+
+Route::get('auth', function() {
+    $credentials = [
+        'email' => 'john@example.com',
+        'password' => 'password'
+    ];
+
+    if(! Auth::attempt($credentials)) {
+        return 'Incorrect username and password combination';
+    }
+
+    return redirect('protected');
+});
+
+Route::get('auth/logout', function() {
+    Auth::logout();
+
+    return 'See you again~';
+});
+
+Route::get('protected', function() {
+    if(! Auth::check()) {
+        return 'Illegal access !!! Get out of here~';
+    }
+
+    return 'Welcome back, '.Auth::user()->name;
+});
